6374c33f68733487c089665d4826578d16f088c5
brave <bravegiantcreative@gmail.com>
2026-07-28 11:57:12 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,5 +2,9 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "berachain/bera-reth"
-REPO_NAME = "beacon-reth"
+SOURCE_REPO = "near/core-contracts"
+REPO_NAME = "core-contracts"
+TREE = ""
+BRANCH = ""
+# Example:
+# https://github.com/pushchain/push-chain-node/tree/0648551281dada6e300f51baca0e7464cb210eef/
