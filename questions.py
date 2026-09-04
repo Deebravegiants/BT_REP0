f3b999d19ca74a5149c144705073f5459363578c
brave <bravegiantcreative@gmail.com>
2026-09-04 18:04:19 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-MAX_REPO = 4
-SOURCE_REPO = "Hinkal-Protocol/Hinkal-Contracts-Circuits"
-REPO_NAME = "Hinkal-Contracts-Circuits"
+MAX_REPO = 25
+SOURCE_REPO = "logos-messaging/logos-delivery-js"
+REPO_NAME = "logos-delivery-js"
 TREE = ""
 BRANCH = ""
 # Example:
